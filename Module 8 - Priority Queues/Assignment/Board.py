7c651302db8e858be4161e7c00161d5e0665e5b5
Marcos Peixoto <marcosferreirapeixoto@gmail.com>
2024-12-03 20:24:46 -0300

    Finishing board class and creating tests

--- Module 8 - Priority Queues/Assignment/Board.py
+++ Module 8 - Priority Queues/Assignment/Board.py
@@ -1,5 +1,6 @@
 from typing import List
-from copy import copy
+from copy import deepcopy
+from random import choice
 
 class Board():
     def __init__(self, matrix: List[List[int]]):
@@ -15,7 +16,7 @@ class Board():
 
         for i in range(self.n):
             for j in range(self.n):
-                if self._board[i][j] != self._goal_board[i][j]:
+                if self._board[i][j] != 0 and self._board[i][j] != self._goal_board[i][j]:
                     hamming += 1
 
         return hamming
@@ -45,30 +46,44 @@ class Board():
         i, j = self._get_zero_position()
 
         if i != 0:
-            neighbor_matrix = copy(self._board)
-            neighbor_matrix[i][j], neighbor_matrix[i-1][j] = neighbor_matrix[i][j], neighbor_matrix[i-1][j]
+            neighbor_matrix = deepcopy(self._board)
+            neighbor_matrix[i][j], neighbor_matrix[i-1][j] = neighbor_matrix[i-1][j], neighbor_matrix[i][j]
 
             neighbors.append(Board(neighbor_matrix))
 
-        if i != self.n:
-            neighbor_matrix = copy(self._board)
-            neighbor_matrix[i][j], neighbor_matrix[i+1][j] = neighbor_matrix[i][j], neighbor_matrix[i+1][j]
+        if i != (self.n - 1):
+            neighbor_matrix = deepcopy(self._board)
+            neighbor_matrix[i][j], neighbor_matrix[i+1][j] = neighbor_matrix[i+1][j], neighbor_matrix[i][j]
 
             neighbors.append(Board(neighbor_matrix))
         
         if j != 0:
-            neighbor_matrix = copy(self._board)
+            neighbor_matrix = deepcopy(self._board)
             neighbor_matrix[i][j], neighbor_matrix[i][j-1] = neighbor_matrix[i][j-1], neighbor_matrix[i][j]
 
             neighbors.append(Board(neighbor_matrix))
 
-        if i != self.n:
-            neighbor_matrix = copy(self._board)
-            neighbor_matrix[i][j], neighbor_matrix[i+1][j] = neighbor_matrix[i][j], neighbor_matrix[i+1][j]
+        if j != (self.n - 1):
+            neighbor_matrix = deepcopy(self._board)
+            neighbor_matrix[i][j], neighbor_matrix[i][j+1] = neighbor_matrix[i][j + 1], neighbor_matrix[i][j]
 
             neighbors.append(Board(neighbor_matrix))
         
+        return neighbors
 
+    def twin(self):
+        non_zero_positions = [
+            (i, j) for i in range(self.n) for j in range(self.n) if self._board[i][j] != 0
+        ]
+
+        (i1, j1), (i2, j2) = choice(non_zero_positions), choice(non_zero_positions)
+        while (i1, j1) == (i2, j2):
+            (i2, j2) = choice(non_zero_positions)
+
+        twin_board = deepcopy(self._board)
+        twin_board[i1][j1], twin_board[i2][j2] = twin_board[i2][j2], twin_board[i1][j1]
+
+        return Board(twin_board)
 
     def _get_zero_position(self):
         for i in range(self.n):
@@ -76,7 +91,6 @@ class Board():
                 if self._board[i][j] == 0:
                     return i, j        
         
-
     def _generate_goal_board(self):
         nums = [0] + list(range(self.n**2 - 1, 0, -1))
         
@@ -84,8 +98,10 @@ class Board():
 
     def __eq__(self, other: 'Board'):
         if not isinstance(other, Board):
-            raise TypeError("Must compare boards!!")
+            return False
         return self._board == other._board
     
     def __str__(self):
-        return "\n".join(" ".join(map(str, row)) for row in self._board)
+        result = f"{self.n}\n"
+        result += "\n".join(" ".join(map(str, row)) for row in self._board)
+        return result
